7cb4f585ad7fe89df08247f0115421c37ece7d89
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2022-08-07 22:29:58 +0300

    power 373

--- src/test_power.py
+++ src/test_power.py
@@ -14,16 +14,16 @@ def test_check_power(files):
 # middle
 
 
-@pytest.mark.parametrize("files", load_files_for_power('../3.Power'))
-def test_power_linear_log(files):
-    number, power, out = files
-    res = power_linear_log(number, power)
-    assert "{:.7f}".format(res) == "{:.7f}".format(out)
+# @pytest.mark.parametrize("files", load_files_for_power('../3.Power'))
+# def test_power_linear_log(files):
+#     number, power, out = files
+#     res = power_linear_log(number, power)
+#     assert "{:.7f}".format(res) == "{:.7f}".format(out)
 
 
-@pytest.mark.parametrize("files", load_files_for_power('../3.Power'))
-def test_power_log(files):
-    number, power, out = files
-    res = power_log(number, power)
-    # setcontext(Context(prec=11, rounding=ROUND_DOWN))
-    assert "{:.6f}".format(res) == "{:.6f}".format(out)
+# @pytest.mark.parametrize("files", load_files_for_power('../3.Power'))
+# def test_power_log(files):
+#     number, power, out = files
+#     res = power_log(number, power)
+#     # setcontext(Context(prec=11, rounding=ROUND_DOWN))
+#     assert "{:.6f}".format(res) == "{:.6f}".format(out)
