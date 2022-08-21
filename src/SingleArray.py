84c538fb53114253dae2d8e50f3d62dbf652f67a
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2022-08-21 10:47:41 +0300

    to python

---
+++ src/SingleArray.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+
+class SingleArray:
+    def __init__(self) -> None:
+        self.arr = []
+
+    def add(item: Any, index: int) -> None:
+        pass
+
+    def remove(index: int) -> Any:
+        pass
