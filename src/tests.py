5a9bee1f1ca238f5aac1bdef74b0434e0737b717
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2022-08-21 17:02:34 +0300

    with single aray test

--- src/tests.py
+++ src/tests.py
@@ -29,7 +29,7 @@ def test_single_array_add(item: Any, ind: int, single_array: SingleArray):
     old_len = single_array.len
     single_array.add(item, ind)
     assert single_array.arr[ind] == item
-    assert single_array.len >old_len
+    assert single_array.len > old_len
     print(single_array.arr)
 
 
@@ -39,3 +39,14 @@ def test_single_array_remove(ret_value: Any, ind: int, single_array: SingleArray
     assert single_array.remove(ind) == ret_value
     assert single_array.len == old_len-1
     print(single_array.arr)
+
+
+@pytest.mark.parametrize("range_val", [100, 1_000, 10_000, 100_000, 1_000_000])
+def test_single_array(range_val):
+    arr = SingleArray()
+    some_value = 20
+    base_index = 0
+    for i in range(range_val):
+        arr.add(some_value, base_index)
+
+    assert arr.len == range_val
