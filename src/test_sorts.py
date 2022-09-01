b1fe97d1f179affbca60294162ceea46f623672c
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2022-09-01 15:10:01 +0300

    with algo tables

--- src/test_sorts.py
+++ src/test_sorts.py
@@ -1,12 +1,18 @@
 import pytest
-from sorts_algo import *
+from merge_sort import *
 
 from load_data import *
+from quick_sort import QuickSort
 
-path = '../src/sorting-tests/0.random'
+path = '../src/sorting-tests/3.revers'
 
 
 @pytest.mark.parametrize("files", load_array(path))
 def test_merge_sort(files):
     size, input, output = files
     assert mergeSort(input) == output
+
+@pytest.mark.parametrize("files", load_array(path))
+def test_quick_sort(files):
+    size, input, output = files
+    assert QuickSort(input,size).sort() == output
